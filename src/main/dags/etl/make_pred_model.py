a9a8bd91f4c30ad5c78f5c1535a5291a20a7b666
alireza <sdamoosavi@gmail.com>
2020-10-01 22:11:42 +0330

    minutely dag ml part is initialized

--- src/main/dags/etl/make_pred_model.py
+++ src/main/dags/etl/make_pred_model.py
@@ -29,7 +29,7 @@ def develop_pred_model_v2(hdfs_master, hdfs_path, run_time, **kwargs):
     )
 
     logger.info("READing from hdfs")
-    _path = os.path.join(*[hdfs_master, hdfs_path, "tesla_stock_data"])
+    _path = os.path.join(*[hdfs_master, hdfs_path, "real_time_tesla_stock_data"])
     _df = spark.read.format("csv").option("header", "true").csv(f"{_path}/*.csv")
 
     _df.show()
@@ -37,25 +37,19 @@ def develop_pred_model_v2(hdfs_master, hdfs_path, run_time, **kwargs):
 
     # _df = _df.withColumn("date", unix_timestamp("date", format="yyyy-MM-dd HH:mm:ss"))
 
-    _df = _df.withColumn("close", _df["close"].cast("float").alias("close"))
-    _df = _df.withColumn("open", _df["open"].cast("float").alias("open"))
+    _df = _df.withColumn("price", _df["price"].cast("float").alias("price"))
     _df = _df.withColumn("date", _df["date"].cast("timestamp").alias("date"))
 
     _df.printSchema()
     _df = _df.orderBy("date", ascending=True)
     _df.show()
 
-
     logger.info("df to pandas:")
     pd_df = _df.toPandas()
     pd_df.dropna(inplace=True)
-    pd_df["date"] = pd_df.to_datetime(pd_df.date)
-    
-    pd_df.sort_values(by=["date"], inplace=True, ascending=True)
-    pd_df.reset_index(inplace=True)
 
     logger.info("SHAPE is: ")
-    logger.info(pd_df.shape)
+    logger.info(pd_df.head(8))
 
     # _df.withColumn("new_column", lag("num", 1, 0).over(w)).show lag(col, count=1, default=None)
 
@@ -74,6 +68,16 @@ def develop_pred_model(hdfs_master, hdfs_path, run_time, **kwargs):
     logger.info("READing from hdfs")
     _path = os.path.join(*[hdfs_master, hdfs_path, "tesla_stock_data"])
     _df = spark.read.format("csv").option("header", "true").csv(f"{_path}/*.csv")
+
+    # _df = _df.withColumn("date", unix_timestamp("date", format="yyyy-MM-dd HH:mm:ss"))
+
+    _df = _df.withColumn("close", _df["close"].cast("float").alias("close"))
+    _df = _df.withColumn("open", _df["open"].cast("float").alias("open"))
+    _df = _df.withColumn("date", _df["date"].cast("timestamp").alias("date"))
+
+    _df.printSchema()
+    _df = _df.orderBy("date", ascending=True)
+    _df.show()
 
     _df.show()
     _df.printSchema()
